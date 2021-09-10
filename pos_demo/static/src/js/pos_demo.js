de6da2f9ccdf4904b2e7ed57a21b661da0a42714
rsmarcos26 <59517898+rsmarcos26@users.noreply.github.com>
2021-09-10 16:54:50 -0500

    se agregó el botón de listar pedidos recientes

--- pos_demo/static/src/js/pos_demo.js
+++ pos_demo/static/src/js/pos_demo.js
@@ -1,10 +1,14 @@
+//Se confirma la carga del JS
 console.log('JavaScript cargado correctamente');
 
+//Agregar botón de descuento
 odoo.define('pos_demo.custom', function (require) {
     "use strict";
+    //Se crean constantes que se enlacan a los componentes
     const PosComponent = require('point_of_sale.PosComponent');
     const ProductScreen = require('point_of_sale.ProductScreen');
     const Registries = require('point_of_sale.Registries');
+    //Funcionalidad del botón
     class PosDiscountButton extends PosComponent {
         async onClick() {
             const order = this.env.pos.get_order();
@@ -13,13 +17,67 @@ odoo.define('pos_demo.custom', function (require) {
             }
         }
     }
+    //Se elige el template para el botón
     PosDiscountButton.template = 'PosDiscountButton';
+    //Se muestra el botón en pantalla
     ProductScreen.addControlButton({
         component: PosDiscountButton,
         condition: function () {
             return true;
         },
     });
+    //Se registra el botón en los componentes
     Registries.Component.add(PosDiscountButton);
-    return PosDiscountButton;
+
+
+
+    //Funcionalidad para listar los  últimos 5 pedidos
+    class PosLastOrderButton extends PosComponent {
+        async onClick() {
+            var self = this;
+            const order = this.env.pos.get_order();
+            if (order.attributes.client) {
+                var domain = [['partner_id', '=', order.
+                    attributes.client.id]];
+                this.rpc({
+                    model: 'pos.order', method: 'search_read',
+                    args: [domain, ['name', 'amount_total']],
+                    kwargs: { limit: 5 },
+                }).then(function (orders) {
+                    if (orders.length > 0) {
+                        var order_list = _.map(orders, function
+                            (o) {
+                            return {
+                                'label': _.str.sprintf("%s - TOTAL: % s", o.name, o.amount_total)
+                            };
+                        });
+                        self.showPopup('SelectionPopup', {
+                            title:
+                                'Last 5 orders', list: order_list
+                        });
+                    } else {
+                        self.showPopup('ErrorPopup', {
+                            body: 'No previous orders found'
+                        });
+                    }
+                });
+            } else {
+                self.showPopup('ErrorPopup', {
+                    body: 'Please select the customer'
+                });
+            }
+        }
+    }
+    PosLastOrderButton.template = 'PosLastOrderButton';
+    ProductScreen.addControlButton({
+        component: PosLastOrderButton,
+        condition: function () {
+            return true;
+        },
+    });
+    Registries.Component.add(PosLastOrderButton);
+    return PosDiscountButton, PosLastOrderButton;
+
 });
+
+
